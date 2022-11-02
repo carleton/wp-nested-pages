c8772b7a6fda112b2a94cc23dde270a82d9dab18
Kyle Veldhuizen <kveldhuizen@carleton.edu>
2022-11-02 08:58:01 -0500

    Including the vendor folder as it is not part of the source code rather it's probably added as part of the CI/CD process

---
+++ vendor/composer/installed.php
@@ -0,0 +1,44 @@
+<?php return array(
+    'root' => array(
+        'name' => 'carleton-college/wp-nested-pages',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'a8c4a260ef23dc57e2c541ea5c1fdb9cbfe43800',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'carleton-college/wp-nested-pages' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'a8c4a260ef23dc57e2c541ea5c1fdb9cbfe43800',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'composer/installers' => array(
+            'pretty_version' => 'v1.12.0',
+            'version' => '1.12.0.0',
+            'reference' => 'd20a64ed3c94748397ff5973488761b22f6d3f19',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/./installers',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'roundcube/plugin-installer' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'shama/baton' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+    ),
+);
